4596bbec61b8a63c718c467b3f189bc2d2e71ac9
Hafiz Muhammad Ali Sajjad <alisajjad251992@gmail.com>
2024-01-06 22:00:19 +0100

    signup form setup

--- src/autostore/settings.py
+++ src/autostore/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'localflavor',
+    'crispy_forms',
+    'crispy_bootstrap4',
     'main',
     'users',
 ]
@@ -126,6 +128,9 @@ STATIC_URL = 'static/'
 #uploaded media files
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- src/autostore/urls.py
+++ src/autostore/urls.py
@@ -19,12 +19,14 @@ from django.urls import path, include
 from main import urls as main_app_urls
 from django.conf import settings
 
+from users import urls as users_app_urls
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(main_app_urls))
+    path('', include(main_app_urls)),
+    path('', include(users_app_urls),)
 ]
 
 if settings.DEBUG:
