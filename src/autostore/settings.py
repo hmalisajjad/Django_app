84fff9ad10aaf8f1c538686360571b479b0e8bf3
Hafiz Muhammad Ali Sajjad <alisajjad251992@gmail.com>
2024-01-07 17:46:39 +0100

    setting_environ_functionality

--- src/autostore/settings.py
+++ src/autostore/settings.py
@@ -11,9 +11,14 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
-
+import environ
 
 from django.contrib.messages import constants as messages
+
+#Initialize Environ
+env = environ.Env()
+env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,10 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ap9$58!-6$m)25@@zn&wock6vx6kf2%6=#%5yb=yw1)3!55v5n'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(env('DEBUG'))
 
 ALLOWED_HOSTS = ['*']
 
