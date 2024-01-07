90345cbf10f6896b212c3ddafe48bfd8d6334e4f
Hafiz Muhammad Ali Sajjad <alisajjad251992@gmail.com>
2024-01-07 13:48:50 +0100

    setting_search_filters

--- src/main/views.py
+++ src/main/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import Listing
 from .forms import ListingForm
 from users.forms import LocationForm
+from .filters import ListingFilter
 
 
 def main_view(request):
@@ -13,15 +15,33 @@ def main_view(request):
 
 def home_view(request):
     listings = Listing.objects.all()
+    listing_filter = ListingFilter(request.GET, queryset=listings)
     context = {
-        'listing': listings,
+        'listing_filter': listing_filter
     }
     return render(request, "views/home.html", context)
 
 @login_required
 def list_view(request):
     if request.method == 'POST':
-        pass
+        try:
+            listing_form = ListingForm(request.POST, request.FILES)
+            location_form = LocationForm(request.POST, )
+            if listing_form.is_valid() and location_form.is_valid():
+                listing = listing_form.save(commit=False)
+                listing_location = location_form.save()
+                listing.seller = request.user.profile
+                listing.location = listing_location
+                listing.save()
+                messages.info(
+                    request, f'{listing.model} Listing Posted Successfully!')
+                return redirect('home')
+            else:
+                raise Exception()
+        except Exception as e:
+            print(e)
+            messages.error(
+                request, 'An error occured while posting the listing.')
     elif request.method == 'GET':
         listing_form = ListingForm()
         location_form = LocationForm()
