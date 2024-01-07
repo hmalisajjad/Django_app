9799a12b75ffa7ef838498116f0e755956f2a436
Hafiz Muhammad Ali Sajjad <alisajjad251992@gmail.com>
2024-01-07 10:45:24 +0100

    basics_done

--- src/users/views.py
+++ src/users/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect
 from django.contrib import messages
+from django.views import View
 
 def login_view(request):
     if request.method == 'POST':
@@ -23,6 +24,21 @@ def login_view(request):
         login_form = AuthenticationForm()
     return render(request,'views/login.html', {'login_form': login_form})
 
-def register_view(request):
-     register_form = UserCreationForm()
-     return render(request, 'views/register.html', {'register_form': register_form})
+class RegisterView(View):
+
+    def get(self, request):
+        register_form = UserCreationForm()
+        return render(request, 'views/register.html', {'register_form': register_form})
+
+    def post(self, request):
+        register_form = UserCreationForm(request.POST)
+        if register_form.is_valid():
+            user = register_form.save()
+            user.refresh_from_db()
+            login(request, user)
+            messages.success(
+                request, f'User {user.username} registered successfully.')
+            return redirect('home')
+        else:
+            messages.error(request, f'An error occured trying to register.')
+            return render(request, 'views/register.html', {'register_form': register_form})
